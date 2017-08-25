b5826e5e14b7c0dd603dc415dbbc419dc517f597
Ara <ayaghsizian@gmail.com>
2017-08-25 13:49:31 -0400

    day 1 complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Day 0
 // console.log("Hello, World.");
+
+// Day 1
+// console.log(parseInt(input_stdin_array[0]) + i);
+//
+//
+// var d1 = parseFloat(input_stdin_array[1]) + parseFloat(d);
+// if( d1.toString().length == 1 )
+// {
+//    console.log(d1 + ".0");
+// }
+// else
+// {
+//     console.log(d1);
+// }
+//
+//
+// console.log(s + input_stdin_array[2]);
