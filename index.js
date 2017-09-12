bd23bb5f065d3a7513a2fe38975ee63b97f575dc
Ara <ayaghsizian@gmail.com>
2017-09-12 19:37:09 -0400

    working on question 12 syntax error somewhere

--- index.js
+++ index.js
@@ -22,30 +22,59 @@
 // my code disappeared :(
 // I'll save future challenges before I click next
 
-Day 4
-function Person(initialAge){
+// Day 4
+// function Person(initialAge){
+//
+//     if( initialAge<0 ){
+//         console.log('Age is not valid, setting age to 0.');
+//         this.age = 0;
+//     }
+//     else if( initialAge <= 30 ){
+//         this.age = initialAge;
+//     }
+//
+//     this.amIOld=function(){
+//       if(this.age < 13){
+//           console.log("You are young.");
+//       }
+//       else if(this.age < 18){
+//           console.log('You are a teenager.');
+//       }
+//       else{
+//       console.log("You are old.");
+//       }
+//     };
+//
+//     this.yearPasses=function(){
+//       this.age += 1;
+//     };
+// }//person
 
-    if( initialAge<0 ){
-        console.log('Age is not valid, setting age to 0.');
-        this.age = 0;
+// Day 12
+class Student extends Person {
+    /*
+    *   Class Constructor
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    constructor(scores) {
+        super(scores);
     }
-    else if( initialAge <= 30 ){
-        this.age = initialAge;
-    }
-
-    this.amIOld=function(){
-      if(this.age < 13){
-          console.log("You are young.");
-      }
-      else if(this.age < 18){
-          console.log('You are a teenager.');
-      }
-      else{
-      console.log("You are old.");
-      }
-    };
-
-    this.yearPasses=function(){
-      this.age += 1;
-    };
-}//person
+    /*
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    this.calculate = function(scores){
+        var total = 0;
+        scores.forEach((score)=>{
+            total += score;
+        });
+        var avg = total/scores.length;
+        if(avg > 0){
+            console.log('progress!');
+        return 'p';
+        }//if
+    }//calc
+}//student
